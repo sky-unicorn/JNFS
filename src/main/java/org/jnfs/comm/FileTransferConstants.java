7b8b4e6fa9e2c8086c5f76446ce26ef4d4669238
liutianlin4431 <65290075+liutianlin4431@users.noreply.github.com>
2025-11-05 16:53:12 +0800

    使用 netty nio 通过零拷贝技术实现文件上传与下载

--- src/main/java/org/jnfs/comm/FileTransferConstants.java
+++
@@ -1,9 +0,0 @@
-package org.jnfs.comm;
-
-// FileTransferConstants.java
-public class FileTransferConstants {
-    public static final int PORT = 8888;
-    public static final String SERVER_HOST = "localhost";
-    public static final int MAX_FRAME_LENGTH = 1024 * 1024 * 100; // 100MB
-    public static final String UPLOAD_DIR = "C:\\Users\\liuti\\Desktop\\test\\";
-}
